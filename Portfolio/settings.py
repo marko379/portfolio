4c991d8e8a43cbaec77a7964fe1fcefd9d915576
marko <markogojkovi045@gmail.com>
2024-08-31 13:39:15 +0100

    fixed email

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -160,10 +160,3 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'markogojkovic045@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = ''
